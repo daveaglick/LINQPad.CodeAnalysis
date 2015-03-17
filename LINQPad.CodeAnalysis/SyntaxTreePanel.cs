3805dd0452a8f7a42837f36b2de239c2b2a33ee7
Dave Glick <dave@somedave.com>
2015-03-17 09:46:21 -0400

    Declaration filter now works for VB

--- LINQPad.CodeAnalysis/SyntaxTreePanel.cs
+++ LINQPad.CodeAnalysis/SyntaxTreePanel.cs
@@ -199,25 +199,49 @@ namespace LINQPad.CodeAnalysis
             _treeList.ExpandAll();
         }
 
-        private static bool SyntaxNodeMatchesDeclaration(SyntaxNode syntaxNode, string declarationFilter)
+        private bool SyntaxNodeMatchesDeclaration(SyntaxNode syntaxNode, string declarationFilter)
         {
             if (string.IsNullOrWhiteSpace(declarationFilter))
             {
                 return true;
             }
 
-            // This is a hack, but don't know any other way to check for identifiers across all syntax node types - YOLO!
-            if (SyntaxNodeWrapper.Get(syntaxNode).GetKind().EndsWith("Declaration"))
+            // Using reflection to get .Identifier is a hack, but don't know any other way to check for identifiers across all syntax node types - YOLO!
+            // Also, the semantics of CSharp and Visual Basic appear to be different (I.e., VB puts everything inside a "Block")
+            if (_syntaxTree is Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree)
             {
-                PropertyInfo identifierProperty = syntaxNode.GetType().GetProperty("Identifier", BindingFlags.Public | BindingFlags.Instance);
-                if (identifierProperty != null)
+                if (SyntaxNodeWrapper.Get(syntaxNode).GetKind().EndsWith("Declaration"))
                 {
-                    object identifierToken = identifierProperty.GetValue(syntaxNode);
-                    return identifierToken != null && identifierToken is SyntaxToken && ((SyntaxToken)identifierToken).ValueText == declarationFilter;
+                    return GetIdentifierTokenValueText(syntaxNode) == declarationFilter;
+                }
+            }
+            else if(_syntaxTree is Microsoft.CodeAnalysis.VisualBasic.VisualBasicSyntaxTree)
+            {
+                if (SyntaxNodeWrapper.Get(syntaxNode).GetKind().EndsWith("Block"))
+                {
+                    SyntaxNode firstChild = syntaxNode.ChildNodes().FirstOrDefault();
+                    if (firstChild != null && SyntaxNodeWrapper.Get(firstChild).GetKind().EndsWith("Statement"))
+                    {
+                        return GetIdentifierTokenValueText(firstChild) == declarationFilter;
+                    }
                 }
             }
 
             return false;
+        }
+
+        private static string GetIdentifierTokenValueText(SyntaxNode syntaxNode)
+        {
+            PropertyInfo identifierProperty = syntaxNode.GetType().GetProperty("Identifier", BindingFlags.Public | BindingFlags.Instance);
+            if (identifierProperty != null)
+            {
+                object identifierToken = identifierProperty.GetValue(syntaxNode);
+                if (identifierToken != null && identifierToken is SyntaxToken)
+                {
+                    return ((SyntaxToken)identifierToken).ValueText;
+                }
+            }
+            return null;
         }
 
         private class SyntaxFilter : IModelFilter
