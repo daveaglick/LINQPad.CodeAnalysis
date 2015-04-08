32cb13e186d0553831fcaca0d068dc840903fe8a
Dave Glick <dave@daveaglick.com>
2015-04-08 12:21:58 -0400

    Syntax graph now supports filtering and multiple trees with multiple declaration filter matches - #6

--- LINQPad.CodeAnalysis/SyntaxTreePanel.cs
+++ LINQPad.CodeAnalysis/SyntaxTreePanel.cs
@@ -19,13 +19,21 @@ namespace LINQPad.CodeAnalysis
 {
     internal class SyntaxTreePanel : TableLayoutPanel
     {
+        // Controls must be initialized in declaration order
+        private TextBox _textBox;
+        private GViewer _graphViewer;
+        private TreeListView _treeList;
+        private ToolStrip _toolStrip;
+        private CheckBox _syntaxTokenCheckBox;
+        private CheckBox _syntaxTriviaCheckBox;
+
         public SyntaxTreePanel(SyntaxTree syntaxTree, string declarationFilter)
         {
             // Controls
-            TextBox textBox = CreateTextBox();
-            GViewer graphViewer = CreateGraphViewer();
-            TreeListView treeList = CreateTreeList(textBox, graphViewer, syntaxTree, declarationFilter);
-            ToolStrip toolStrip = CreateToolStrip(treeList, textBox, graphViewer, syntaxTree, declarationFilter);
+            CreateTextBox();
+            CreateGraphViewer();
+            CreateTreeList(syntaxTree, declarationFilter);
+            CreateToolStrip(syntaxTree, declarationFilter);
 
             // Right-hand splitter
             SplitContainer rightSplitContainer = new SplitContainer()
@@ -33,8 +41,8 @@ namespace LINQPad.CodeAnalysis
                 Dock = DockStyle.Fill,
                 Orientation = Orientation.Horizontal
             };
-            rightSplitContainer.Panel1.Controls.Add(textBox);
-            rightSplitContainer.Panel2.Controls.Add(graphViewer);
+            rightSplitContainer.Panel1.Controls.Add(_textBox);
+            rightSplitContainer.Panel2.Controls.Add(_graphViewer);
             bool rightSplitVisibleChanged = false;
             rightSplitContainer.VisibleChanged += (x, e) =>
             {
@@ -50,7 +58,7 @@ namespace LINQPad.CodeAnalysis
             {
                 Dock = DockStyle.Fill
             };
-            splitContainer.Panel1.Controls.Add(treeList);
+            splitContainer.Panel1.Controls.Add(_treeList);
             splitContainer.Panel2.Controls.Add(rightSplitContainer);
             bool splitVisibleChanged = false;
             splitContainer.VisibleChanged += (x, e) =>
@@ -61,24 +69,23 @@ namespace LINQPad.CodeAnalysis
                     splitContainer.SplitterDistance = (int)(splitContainer.ClientSize.Width * 0.5);
                 }
             };
-            Controls.Add(toolStrip, 0, 0);
+            Controls.Add(_toolStrip, 0, 0);
             Controls.Add(splitContainer, 0, 1);
         }
 
-        private static TextBox CreateTextBox()
+        private void CreateTextBox()
         {
-            TextBox textBox = new TextBox()
+            _textBox = new TextBox()
             {
                 Multiline = true,
                 Dock = DockStyle.Fill,
                 Font = InternalsHelper.GetDefaultFont()
             };
-            return textBox;
         }
 
-        private static GViewer CreateGraphViewer()
+        private void CreateGraphViewer()
         {
-            GViewer graphViewer = new GViewer
+            _graphViewer = new GViewer
             {
                 Dock = DockStyle.Fill, 
                 OutsideAreaBrush = Brushes.White,
@@ -88,13 +95,12 @@ namespace LINQPad.CodeAnalysis
                 SaveAsMsaglEnabled = false,
                 UndoRedoButtonsVisible = false
             };
-            return graphViewer;
         }
 
-        private static TreeListView CreateTreeList(TextBox textBox, GViewer graphViewer, SyntaxTree syntaxTree, string declarationFilter)
+        private void CreateTreeList(SyntaxTree syntaxTree, string declarationFilter)
         {
             // Create the tree view
-            TreeListView treeList = new TreeListView()
+            _treeList = new TreeListView()
             {
                 CanExpandGetter = x => ((SyntaxWrapper)x).CanExpand(),
                 ChildrenGetter = x => ((SyntaxWrapper)x).GetChildren(),
@@ -102,42 +108,42 @@ namespace LINQPad.CodeAnalysis
                 Dock = DockStyle.Fill,
                 UseFiltering = true
             };
-            treeList.FormatCell += (x, e) => ((SyntaxWrapper)e.CellValue).FormatCell(e);
-            treeList.BeforeSorting += (x, e) => e.Canceled = true;
+            _treeList.FormatCell += (x, e) => ((SyntaxWrapper)e.CellValue).FormatCell(e);
+            _treeList.BeforeSorting += (x, e) => e.Canceled = true;
 
             // Select - show the text
-            treeList.SelectedIndexChanged += (x, e) =>
+            _treeList.SelectedIndexChanged += (x, e) =>
             {
-                if (treeList.SelectedItem != null)
+                if (_treeList.SelectedItem != null)
                 {
-                    SyntaxWrapper wrapper = (SyntaxWrapper)treeList.SelectedItem.RowObject;
-                    textBox.Text = wrapper.GetSyntaxObject().ToString();
-                    PopulateGraph(graphViewer, wrapper);
+                    SyntaxWrapper wrapper = (SyntaxWrapper)_treeList.SelectedItem.RowObject;
+                    _textBox.Text = wrapper.GetSyntaxObject().ToString();
+                    PopulateGraph(wrapper);
                 }
             };
 
             // Activate - dump the syntax object
-            treeList.ItemActivate += (x, e) =>
+            _treeList.ItemActivate += (x, e) =>
             {
-                if (treeList.SelectedItem != null)
+                if (_treeList.SelectedItem != null)
                 {
-                    SyntaxWrapper wrapper = (SyntaxWrapper)treeList.SelectedItem.RowObject;
+                    SyntaxWrapper wrapper = (SyntaxWrapper)_treeList.SelectedItem.RowObject;
                     wrapper.GetSyntaxObject().Dump(wrapper.GetKind() + " " + wrapper.GetSpan());
                 }
             };
 
             // Create columns
-            treeList.Columns.Add(new OLVColumn("Kind", null)
+            _treeList.Columns.Add(new OLVColumn("Kind", null)
             {
                 AspectGetter = x => x,
                 AspectToStringConverter = x => ((SyntaxWrapper)x).GetTreeText()
             });
-            treeList.Columns.Add(new OLVColumn("Span", null)
+            _treeList.Columns.Add(new OLVColumn("Span", null)
             {
                 AspectGetter = x => x,
                 AspectToStringConverter = x => ((SyntaxWrapper)x).GetSpan()
             });
-            treeList.Columns.Add(new OLVColumn("Text", null)
+            _treeList.Columns.Add(new OLVColumn("Text", null)
             {
                 AspectGetter = x => x,
                 AspectToStringConverter = x => ((SyntaxWrapper)x).GetText()
@@ -148,63 +154,63 @@ namespace LINQPad.CodeAnalysis
             int depth = 0;
             if (syntaxTree.TryGetRoot(out root))
             {
-                SetRoots(treeList, textBox, graphViewer, syntaxTree, declarationFilter);
+                SetRoots(syntaxTree, declarationFilter);
                 depth = GetDepth(root);
             }
 
             // Calculate control width
-            AutoSizeColumns(treeList, depth, true);
-            treeList.Layout += (x, e) => AutoSizeColumns(treeList, depth, false);
-
-            return treeList;
+            AutoSizeColumns(_treeList, depth, true);
+            _treeList.Layout += (x, e) => AutoSizeColumns(_treeList, depth, false);
         }
 
-        private static ToolStrip CreateToolStrip(TreeListView treeList, TextBox textBox, GViewer graphViewer, SyntaxTree syntaxTree, string declarationFilter)
+        private void CreateToolStrip(SyntaxTree syntaxTree, string declarationFilter)
         {
             // Syntax and trivia toggles
-            CheckBox syntaxTokenCheckBox = new CheckBox()
+            _syntaxTokenCheckBox = new CheckBox()
             {
                 BackColor = Color.Transparent,
                 Checked = true,
             };
-            CheckBox syntaxTriviaCheckBox = new CheckBox()
+            _syntaxTriviaCheckBox = new CheckBox()
             {
                 BackColor = Color.Transparent,
                 Checked = true,
             };
             bool handleChecked = true;	// Prevent double handling from adjustments during another handler
-            syntaxTokenCheckBox.CheckedChanged += (x, e) =>
+            _syntaxTokenCheckBox.CheckedChanged += (x, e) =>
             {
                 if (handleChecked)
                 {
-                    if (!syntaxTokenCheckBox.Checked)
+                    if (!_syntaxTokenCheckBox.Checked)
                     {
                         handleChecked = false;
-                        syntaxTriviaCheckBox.Checked = false;
+                        _syntaxTriviaCheckBox.Checked = false;
                         handleChecked = true;
                     }
-                    if (syntaxTokenCheckBox.Checked && syntaxTriviaCheckBox.Checked)
+                    if (_syntaxTokenCheckBox.Checked && _syntaxTriviaCheckBox.Checked)
                     {
-                        treeList.ModelFilter = null;
+                        _treeList.ModelFilter = null;
                     }
-                    treeList.ModelFilter = new SyntaxFilter(syntaxTokenCheckBox.Checked, syntaxTriviaCheckBox.Checked);
+                    _treeList.ModelFilter = new SyntaxFilter(_syntaxTokenCheckBox.Checked, _syntaxTriviaCheckBox.Checked);
+                    PopulateGraph();
                 }
             };
-            syntaxTriviaCheckBox.CheckedChanged += (x, e) =>
+            _syntaxTriviaCheckBox.CheckedChanged += (x, e) =>
             {
                 if (handleChecked)
                 {
-                    if (!syntaxTokenCheckBox.Checked)
+                    if (!_syntaxTokenCheckBox.Checked)
                     {
                         handleChecked = false;
-                        syntaxTokenCheckBox.Checked = true;
+                        _syntaxTokenCheckBox.Checked = true;
                         handleChecked = true;
                     }
-                    if (syntaxTokenCheckBox.Checked && syntaxTriviaCheckBox.Checked)
+                    if (_syntaxTokenCheckBox.Checked && _syntaxTriviaCheckBox.Checked)
                     {
-                        treeList.ModelFilter = null;
+                        _treeList.ModelFilter = null;
                     }
-                    treeList.ModelFilter = new SyntaxFilter(syntaxTokenCheckBox.Checked, syntaxTriviaCheckBox.Checked);
+                    _treeList.ModelFilter = new SyntaxFilter(_syntaxTokenCheckBox.Checked, _syntaxTriviaCheckBox.Checked);
+                    PopulateGraph();
                 }
             };
 
@@ -218,29 +224,29 @@ namespace LINQPad.CodeAnalysis
             {
                 if (e.KeyCode == Keys.Enter)
                 {
-                    SetRoots(treeList, textBox, graphViewer, syntaxTree, declarationFilterTextBox.Text);
+                    SetRoots(syntaxTree, declarationFilterTextBox.Text);
                 }
             };
             declarationFilterTextBox.LostFocus += (x, e) =>
             {
-                SetRoots(treeList, textBox, graphViewer, syntaxTree, declarationFilterTextBox.Text);
+                SetRoots(syntaxTree, declarationFilterTextBox.Text);
             };
 
             // Layout
-            ToolStrip toolStrip = new ToolStrip(
-                new ToolStripButton("Expand All", null, (x, e) => treeList.ExpandAll()),
-                new ToolStripButton("Collapse All", null, (x, e) => treeList.CollapseAll()),
+            _toolStrip = new ToolStrip(
+                new ToolStripButton("Expand All", null, (x, e) => _treeList.ExpandAll()),
+                new ToolStripButton("Collapse All", null, (x, e) => _treeList.CollapseAll()),
                 new ToolStripSeparator(),
                 new ToolStripLabel("SyntaxNode  ")
                 {
                     ForeColor = Color.Blue
                 },
-                new ToolStripControlHost(syntaxTokenCheckBox),
+                new ToolStripControlHost(_syntaxTokenCheckBox),
                 new ToolStripLabel("SyntaxToken  ")
                 {
                     ForeColor = Color.Green
                 },
-                new ToolStripControlHost(syntaxTriviaCheckBox),
+                new ToolStripControlHost(_syntaxTriviaCheckBox),
                 new ToolStripLabel("SyntaxTrivia  ")
                 {
                     ForeColor = Color.Maroon
@@ -254,26 +260,25 @@ namespace LINQPad.CodeAnalysis
                 Renderer = new BorderlessToolStripRenderer(),
                 Padding = new Padding(4)
             };
-            toolStrip.Layout += (x, e) => toolStrip.Width = toolStrip.Parent.Width;
-
-            return toolStrip;
+            _toolStrip.Layout += (x, e) => _toolStrip.Width = _toolStrip.Parent.Width;
         }
 
-        public static void SetRoots(TreeListView treeList, TextBox textBox, GViewer graphViewer, SyntaxTree syntaxTree, string declarationFilter)
+        public void SetRoots(SyntaxTree syntaxTree, string declarationFilter)
         {
-            textBox.Text = string.Empty;
-            PopulateGraph(graphViewer, null);
+            _textBox.Text = string.Empty;
+            PopulateGraph(null);
             SyntaxWrapper[] roots = new SyntaxTreeDeclarationFilter(declarationFilter)
                 .GetMatchingSyntaxNodes(syntaxTree)
                 .Select(x => SyntaxWrapper.Get(x))
                 .ToArray();
-            treeList.Roots = roots;
+            _treeList.Roots = roots;
             if(roots.Length != 0)
             {
-                treeList.ExpandAll();
-                treeList.SelectedItem = treeList.GetItem(0);
-                textBox.Text = roots[0].GetSyntaxObject().ToString();
-                PopulateGraph(graphViewer, roots[0]);
+                _treeList.ExpandAll();
+                _textBox.Text = string.Join(
+                    Environment.NewLine + Environment.NewLine + "-" + Environment.NewLine + Environment.NewLine,
+                    roots.Select(x => x.GetSyntaxObject().ToString()));
+                PopulateGraph();
             }
         }
 
@@ -338,24 +343,51 @@ namespace LINQPad.CodeAnalysis
             }
         }
 
-        // TODO: Filter for node types
-        // TODO: white background
-        private static void PopulateGraph(GViewer graphViewer, SyntaxWrapper wrapper)
+        // This populates the graph using the current roots of the tree list
+        private void PopulateGraph()
         {
-            // Setup
-            graphViewer.SuspendLayout();
+            SyntaxWrapper[] roots = _treeList.Roots == null ? null : _treeList.Roots.Cast<SyntaxWrapper>().ToArray();
+            if (roots == null || roots.Length == 0)
+            {
+                PopulateGraph(null);
+                return;
+            }
+
+            _graphViewer.SuspendLayout();
+            Graph graph = new Graph();
+            int id = 0;
+            foreach (SyntaxWrapper root in roots)
+            {
+                id = PopulateGraph(graph, root, id, null) + 1;
+            }
+            _graphViewer.Graph = graph;
+            _graphViewer.ResumeLayout();
+        }
+
+        // This populates the graph using a given root of the tree
+        // If the root is null, then the graph is cleared
+        private void PopulateGraph(SyntaxWrapper wrapper)
+        {
+            _graphViewer.SuspendLayout();
             Graph graph = new Graph();
             if (wrapper != null)
             {
                 PopulateGraph(graph, wrapper, 0, null);
             }
-            graphViewer.Graph = graph;
-            graphViewer.ResumeLayout();
+            _graphViewer.Graph = graph;
+            _graphViewer.ResumeLayout();
         }
 
         // Returns the last used id
-        private static int PopulateGraph(Graph graph, SyntaxWrapper wrapper, int id, string parentId)
+        private int PopulateGraph(Graph graph, SyntaxWrapper wrapper, int id, string parentId)
         {
+            // Check if filtering
+            if ((wrapper.GetSyntaxObject() is SyntaxToken && _syntaxTokenCheckBox != null && !_syntaxTokenCheckBox.Checked)
+                  || (wrapper.GetSyntaxObject() is SyntaxTrivia && _syntaxTriviaCheckBox != null && !_syntaxTriviaCheckBox.Checked))
+            {
+                return id;
+            }
+
             // Create the node
             string nodeId = id.ToString();
             Node node = new Node(nodeId);
